5b883ae1183869ff48b5e2c03c7b83c743849652
abdulwahabahmedkhanyusufzai <absurw@gmail.com>
2024-12-28 14:51:47 +0500

    commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
 	res.cookie("jwt-netflix", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
 		httpsOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not be accessed by JS
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		sameSite: "none", // CSRF attacks cross-site request forgery attacks
 		secure: ENV_VARS.NODE_ENV !== "development",
 	});
 
